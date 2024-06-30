78de8e3de5087d927be4121c04e39f7e1821212a
Wesley Barros <wesleybarros950@gmail.com>
2024-06-30 13:31:59 -0300

    initial files

---
+++ CompanyCruiserFix.cs
@@ -0,0 +1,48 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LobbyCompatibility.Attributes;
+using LobbyCompatibility.Enums;
+
+namespace CompanyCruiserFix
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
+    [LobbyCompatibility(CompatibilityLevel.Everyone, VersionStrictness.None)]
+    public class CompanyCruiserFix : BaseUnityPlugin
+    {
+        public static CompanyCruiserFix Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger { get; private set; } = null!;
+        internal static Harmony? Harmony { get; set; }
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            Patch();
+
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+
+        internal static void Patch()
+        {
+            Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            Logger.LogDebug("Patching...");
+
+            Harmony.PatchAll();
+
+            Logger.LogDebug("Finished patching!");
+        }
+
+        internal static void Unpatch()
+        {
+            Logger.LogDebug("Unpatching...");
+
+            Harmony?.UnpatchSelf();
+
+            Logger.LogDebug("Finished unpatching!");
+        }
+    }
+}
---
+++ Patches/VehicleControlerPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CompanyCruiserFix.Patches
+{
+    [HarmonyPatch(typeof(VehicleController))]
+    public class VehicleControlerPatch
+    {
+        [HarmonyPatch(nameof(VehicleController.GetVehicleInput))]
+        [HarmonyPostfix]
+        private static void GetVehicleInput(VehicleController __instance)
+        {
+            if (__instance.moveInputVector.magnitude <= 0.1f)
+            {
+                __instance.steeringInput = Mathf.Lerp(__instance.steeringInput, 0.0f, Time.deltaTime * 16.0f);
+                __instance.steeringAnimValue = __instance.steeringInput / 3.0f;
+            }
+        }
+    }
+}
